920f5f2392e8fe9f62e631c4a61570441f342ac6
ibsina@gmail.com <ibsina@gmail.com>
2025-06-03 21:29:51 +0700

    sasa

--- aws/7.6/transitgwyconnect-AA-jkt/transitgwy.tf
+++ aws/7.6/transitgwyconnect-AA-jkt/transitgwy.tf
@@ -74,7 +74,7 @@ resource "aws_ec2_transit_gateway_connect" "attachment" {
 resource "aws_ec2_transit_gateway_connect_peer" "example" {
   peer_address = var.fgtport2ip[0]
   #peer_address                  = "10.0.0.21"
-  bgp_asn                       = "7115"
+  bgp_asn                       = "65115"
   transit_gateway_address       = "1.0.0.68"
   inside_cidr_blocks            = ["169.254.120.0/29"]
   transit_gateway_attachment_id = aws_ec2_transit_gateway_connect.attachment.id
@@ -83,7 +83,7 @@ resource "aws_ec2_transit_gateway_connect_peer" "example" {
 # transit gateway connect peer to secondary
 resource "aws_ec2_transit_gateway_connect_peer" "example2" {
   peer_address                  = var.fgt2port2ip[0]
-  bgp_asn                       = "7115"
+  bgp_asn                       = "65115"
   transit_gateway_address       = "1.0.0.69"
   inside_cidr_blocks            = ["169.254.102.0/29"]
   transit_gateway_attachment_id = aws_ec2_transit_gateway_connect.attachment.id
